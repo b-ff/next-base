2292c8442b528f3907b750d8398f8b40c0b0a30b
Slava Biryukov <networmx128bit@gmail.com>
2023-06-17 01:22:27 +0100

    Layout cleanup #2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,37 @@
 import { App } from "@/components/App";
 import { Flex } from "@/components/Flex";
 import { Layout } from "@/components/Layout";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <App>
       <Layout>
         <Flex padding={6} gap={6}>
-          <h1>Let it be index page!</h1>
-          <p>Here will be content of index page</p>
+          <h1>Let be the index page!</h1>
+          <p>Hello, dear fellow!</p>
+          <p>
+            Shall not take be taken a lot of your time — this, is the future!
+            <br />
+            Here you shall see the possibilities of Next.js along with NextAuth
+            and drizzle of PostgreSQL integration.
+            <br /> With a bit of touch of your genius you shall be able to add
+            i18n and your own BFF (backend for frontend) API.
+            <br />
+            Though, if you way to tired of those backend developers who cannot
+            setup their own Swagger or follow the REST guidelines —<br /> you,
+            my dear fellow could make your own complete API and neglect those
+            practices yourself.
+          </p>
+          <p>Shall we begin?</p>
+          <ul>
+            <li>
+              <Link href="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link href="/posts">Posts</Link>
+            </li>
+          </ul>
         </Flex>
       </Layout>
     </App>
